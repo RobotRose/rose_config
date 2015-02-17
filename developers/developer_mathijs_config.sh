12a5a7e249f7c3f393d90a6d91bd5dc242eb16a3
Mathijs de Langen <langen@robot-rose.nl>
2015-02-17 14:14:12 +0100

    updated rosinstall comments

--- developers/developer_mathijs_config.sh
+++ developers/developer_mathijs_config.sh
@@ -4,6 +4,9 @@ export WORKSPACES_FILE=`rospack find rose_config`/workspaces/default.workspace
 # Rosconsole settings
 export ROSCONSOLE_CONFIG_FILE=`rospack find rose_tools`/rosconsole_config_files/custom_rosconsole_levels.config
 
+# .rosinstall location. The root if the locations that are installed by the wstool.
+export ROSINSTALL_ROOT="/home/mathijs/git/rose"
+
 # Source external configurations
 
 # Location
