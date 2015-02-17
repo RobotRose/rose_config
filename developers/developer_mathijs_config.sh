d2b4bb456fb2252693fff331191a80e8b50f77fc
Mathijs de Langen <langen@robot-rose.nl>
2015-02-17 12:10:34 +0100

    use the default workspace

--- developers/developer_mathijs_config.sh
+++ developers/developer_mathijs_config.sh
@@ -1,5 +1,5 @@
 # Workspaces
-export WORKSPACES_FILE=`rospack find rose_config`/workspaces/mathijs.workspace
+export WORKSPACES_FILE=`rospack find rose_config`/workspaces/default.workspace
 
 # Rosconsole settings
 export ROSCONSOLE_CONFIG_FILE=`rospack find rose_tools`/rosconsole_config_files/custom_rosconsole_levels.config
