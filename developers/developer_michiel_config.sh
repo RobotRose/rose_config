95180da23a781abe5b0ee1cc8137182c9e0d5424
Mathijs de Langen <langen@robot-rose.nl>
2015-02-17 14:17:40 +0100

    more useful comment

--- developers/developer_michiel_config.sh
+++ developers/developer_michiel_config.sh
@@ -5,6 +5,7 @@ export WORKSPACES_FILE=`rospack find rose_config`/workspaces/default.workspace
 export ROSCONSOLE_CONFIG_FILE=`rospack find rose_tools`/rosconsole_config_files/custom_rosconsole_levels.config
 
 # .rosinstall location. The root if the locations that are installed by the wstool.
+# If you change the next location, you have to execute first_compile again.
 export ROSINSTALL_ROOT="/home/osch/git/rose"
 
 # Source external configurations
