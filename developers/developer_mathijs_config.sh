05bcbbfd794ecf6f2905b1990209fb6e3ba6af44
Mathijs de Langen <langen@robot-rose.nl>
2015-02-17 14:38:49 +0100

    test on mathijs developer. Set rose_config and rose_tools

--- developers/developer_mathijs_config.sh
+++ developers/developer_mathijs_config.sh
@@ -1,8 +1,14 @@
+# Location of the rose_config package
+export ROSE_CONFIG="/home/mathijs/git/rose/shared/src/rose_config"
+
+# Location of the rose_tools package
+export ROSE_TOOLS="/home/mathijs/git/rose/shared/src/rose_tools"
+
 # Workspaces
-export WORKSPACES_FILE=`rospack find rose_config`/workspaces/default.workspace
+export WORKSPACES_FILE=${ROSE_CONFIG}/workspaces/default.workspace
 
 # Rosconsole settings
-export ROSCONSOLE_CONFIG_FILE=`rospack find rose_tools`/rosconsole_config_files/custom_rosconsole_levels.config
+export ROSCONSOLE_CONFIG_FILE=${ROSE_TOOLS}/rosconsole_config_files/custom_rosconsole_levels.config
 
 # .rosinstall location. The root if the locations that are installed by the wstool.
 # If you change the next location, you have to execute first_compile again.
@@ -11,4 +17,4 @@ export ROSINSTALL_ROOT="/home/mathijs/git/rose"
 # Source external configurations
 
 # Location
-source `rospack find rose_config`/locations/location_rose.config
+source ${ROSE_CONFIG}/locations/location_rose.config
