addab99676b432fba6cd0260a9c4840d1767f977
Mathijs de Langen <langen@robot-rose.nl>
2015-02-17 11:33:44 +0100

    mathijs config with rospack find

--- developers/developer_mathijs_config.sh
+++ developers/developer_mathijs_config.sh
@@ -1,9 +1,10 @@
 # Workspaces
-# export WORKSPACES_FILE="/home/mathijs/git/rose/rose/src/rose_robot_config/workspaces/mathijs.ws"
-export WORKSPACES_FILE="/home/mathijs/git/rose/shared/src/rose_config/workspaces/mathijs.workspace"
+export WORKSPACES_FILE=`rospack find rose_config`/workspaces/mathijs.workspace
 
 # Rosconsole settings
-export ROSCONSOLE_CONFIG_FILE="/home/mathijs/git/rose2_0/shared/custom_rosconsole_levels.config"
+export ROSCONSOLE_CONFIG_FILE=`rospack find rose_tools`/rosconsole_config_files/custom_rosconsole_levels.config
 
 # Source external configurations
+
+# Location
 source `rospack find rose_config`/locations/location_rose.config
