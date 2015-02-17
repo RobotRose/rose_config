6b908ace6da824ae84441db01a4d62f6272dbaab
Mathijs de Langen <langen@robot-rose.nl>
2015-02-17 15:54:14 +0100

    newline at end of line, added extra comment

--- developers/developer_mathijs_config.sh
+++ developers/developer_mathijs_config.sh
@@ -10,8 +10,9 @@ export WORKSPACES_FILE=${ROSE_CONFIG}/workspaces/default.workspace
 # Rosconsole settings
 export ROSCONSOLE_CONFIG_FILE=${ROSE_TOOLS}/rosconsole_config_files/custom_rosconsole_levels.config
 
-# .rosinstall location. The root if the locations that are installed by the wstool.
-# If you change the next location, you have to execute first_compile again.
+# .rosinstall location. The root of the locations that are installed by the wstool.
+# If you change the next location, you have to execute first_compile again, or 
+# copy the current .rosinstall file to this location.
 export ROSINSTALL_ROOT="/home/mathijs/git/rose"
 
 # Source external configurations
