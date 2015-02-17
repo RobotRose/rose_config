26121ef2c8a8a439a563e801b4547c19de45653e
Mathijs de Langen <langen@robot-rose.nl>
2015-02-17 11:19:33 +0100

    created mathijs developer config file and added workspace file

---
+++ developers/developer_mathijs_config.sh
@@ -0,0 +1,9 @@
+# Workspaces
+# export WORKSPACES_FILE="/home/mathijs/git/rose/rose/src/rose_robot_config/workspaces/mathijs.ws"
+export WORKSPACES_FILE="/home/mathijs/git/rose/shared/src/rose_config/workspaces/mathijs.workspace"
+
+# Rosconsole settings
+export ROSCONSOLE_CONFIG_FILE="/home/mathijs/git/rose2_0/shared/custom_rosconsole_levels.config"
+
+# Source external configurations
+source `rospack find rose_config`/locations/location_rose.config
