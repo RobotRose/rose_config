4a0e82f74cbfe7440c4d2196e686e5de870f2d04
Mathijs de Langen <langen@robot-rose.nl>
2015-02-18 11:51:10 +0100

    developer config files changed.

--- developers/developer_okke_config.sh
+++ developers/developer_okke_config.sh
@@ -1,14 +1,21 @@
+# Location of the rose_config package
+export ROSE_CONFIG="/home/okke/git/rose/shared/src/rose_config"
+
+# Location of the rose_tools package
+export ROSE_TOOLS="/home/okke/git/rose/shared/src/rose_tools"
+
 # Workspaces
-export WORKSPACES_FILE=`rospack find rose_config`/workspaces/default.workspace
+export WORKSPACES_FILE=${ROSE_CONFIG}/workspaces/okke.ws
 
 # Rosconsole settings
-export ROSCONSOLE_CONFIG_FILE=`rospack find rose_tools`/rosconsole_config_files/custom_rosconsole_levels.config
+export ROSCONSOLE_CONFIG_FILE=${ROSE_TOOLS}/rosconsole_config_files/custom_rosconsole_levels.config
 
-# .rosinstall location. The root if the locations that are installed by the wstool.
-# If you change the next location, you have to execute first_compile again.
+# .rosinstall location. The root of the locations that are installed by the wstool.
+# If you change the next location, you have to execute first_compile again, or 
+# copy the current .rosinstall file to this location.
 export ROSINSTALL_ROOT="/home/okke/git/rose"
 
 # Source external configurations
 
 # Location
-source `rospack find rose_config`/locations/location_rose.config
+source ${ROSE_CONFIG}/locations/location_rose.config
