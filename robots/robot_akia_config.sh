086b57e065e2d74cebc181093f5a1bd8fabd6913
Mathijs de Langen <langen@robot-rose.nl>
2015-02-18 11:53:33 +0100

    robot config files

--- robots/robot_akia_config.sh
+++ robots/robot_akia_config.sh
@@ -1,20 +1,28 @@
+# Location of the rose_config package
+export ROSE_CONFIG="/home/rose/git/rose/shared/src/rose_config"
+
+# Location of the rose_tools package
+export ROSE_TOOLS="/home/rose/git/rose/shared/src/rose_tools"
+
 # Workspaces
-export WORKSPACES_FILE=`rospack find rose_config`/workspaces/default.workspace
+export WORKSPACES_FILE=${ROSE_CONFIG}/workspaces/rose.ws
 
 # Rosconsole settings
-export ROSCONSOLE_CONFIG_FILE=`rospack find rose_tools`/rosconsole_config_files/custom_rosconsole_levels.config
+export ROSCONSOLE_CONFIG_FILE=${ROSE_TOOLS}/rosconsole_config_files/custom_rosconsole_levels.config
 
 # Setup commands, called in ~/.bashrc
 export SETUP_COMMAND_PC1="$ROSE_SCRIPTS/setup_ROS.sh /opt/ros/hydro/ 10.8.0.1 http://rosepc1:11311 /home/rose/git/"
 export SETUP_COMMAND_PC2="$ROSE_SCRIPTS/setup_ROS.sh /opt/ros/hydro/ 10.8.0.2 http://rosepc1:11311 /home/rose/git/"
 
-# .rosinstall location
-export ROSINSTALL_LOCATION="/home/mathijs/git/rose"
+# .rosinstall location. The root of the locations that are installed by the wstool.
+# If you change the next location, you have to execute first_compile again, or 
+# copy the current .rosinstall file to this location.
+export ROSINSTALL_ROOT="/home/rose/git/rose"
 
 # Source external configurations
 
 # Robot model
-source `rospack find rose_config`/models/model_rose21.config
+source ${ROSE_CONFIG}/models/model_rose21.config
 
 # Location
-source `rospack find rose_config`/locations/location_rose.config
+source ${ROSE_CONFIG}/locations/location_rose.config
