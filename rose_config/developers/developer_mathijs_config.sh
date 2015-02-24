ec7b64cad4a34f8a658bfaf4dc6ab7db628a3fba
Mathijs de Langen <langen@robot-rose.nl>
2015-02-24 13:09:45 +0100

    removed typo

--- rose_config/developers/developer_mathijs_config.sh
+++ rose_config/developers/developer_mathijs_config.sh
@@ -11,7 +11,7 @@ export WORKSPACES_FILE=${ROSE_CONFIG}/workspaces/mathijs.ws
 export ROSCONSOLE_CONFIG_FILE=${ROSE_TOOLS}/rosconsole_config_files/custom_rosconsole_levels.config
 
 # .rosinstall location. The root of the locations that are installed by the wstool.
-# If you change the next location, you have to remote the old .rosinstall at the current location. Secondly,
+# If you change the next location, you have to remove the old .rosinstall at the current location. Secondly,
 # one should run update_rosinstall.sh from the scripts folder
 export ROSINSTALL_ROOT="/home/mathijs/git/rose"
 
