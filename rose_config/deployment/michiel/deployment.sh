8165d95196493a648abba188f4e917f38f38adc7
ohendriks <hendriks@robot-rose.nl>
2015-03-25 14:46:23 +0100

    Added loy, mathijs, michiel configs

---
+++ rose_config/deployment/michiel/deployment.sh
@@ -0,0 +1,23 @@
+# Repositories location
+export REPOS_LOCATION="git/rose"
+
+# Name a.k.a. serial number of the robot
+export ROBOT_NAME="rose"
+
+# Installation
+export ROBOT_INSTALLATION="michiel"
+
+# Model
+export ROBOT_MODEL="rose21"
+
+# Location
+export ROBOT_LOCATION="rosebv"
+
+# Configuration
+export ROBOT_CONFIG="rose"
+
+# Launch files
+export ROBOT_LAUNCH="rose21"
+
+# Logging
+export ROBOT_LOGGING="default"
