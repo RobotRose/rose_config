3c8ca9deb14862de5bb71017ef3c74ef15d0c2c3
ohendriks <hendriks@robot-rose.nl>
2015-04-08 20:25:56 +0200

    Location configs fixed

--- rose_config/locations/rosebv/location.sh
+++ rose_config/locations/rosebv/location.sh
@@ -2,4 +2,4 @@
 # Rose BV, Horsten 1, Eindhoven
 
 export LOCALIZATION_MAP=${ROSE_CONFIG}/maps/rosebv_big/localization.yaml
-export NAVIGATION_MAP=${ROSE_CONFIG}/maps/rosebv_big/planner.yaml
+export NAVIGATION_MAP=${ROSE_CONFIG}/maps/rosebv_big/navigation.yaml
