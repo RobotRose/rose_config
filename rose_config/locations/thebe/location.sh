60aef6381cdefc48b4fb1746c6d22afdc4606dde
ohendriks <hendriks@robot-rose.nl>
2015-04-08 20:19:09 +0200

    Location configs fixed

--- rose_config/locations/thebe/location.sh
+++ rose_config/locations/thebe/location.sh
@@ -1,5 +1,5 @@
 # Thebe | De Hillen
 # Heksenwiellaan 52, Breda
 
-export LOCALIZATION_MAP=${ROSE_CONFIG}/maps/thebe/localization
-export NAVIGATION_MAP=${ROSE_CONFIG}/maps/thebe/navigation
+export LOCALIZATION_MAP=${ROSE_CONFIG}/maps/thebe/localization.yaml
+export NAVIGATION_MAP=${ROSE_CONFIG}/maps/thebe/navigation.yaml
