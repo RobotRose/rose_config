0696648857ab0d73b1b033938f1fa48e96a05063
ohendriks <hendriks@robot-rose.nl>
2015-04-17 10:10:45 +0200

    Added thebe_heuvel location

---
+++ rose_config/locations/thebe_heuvel/location.sh
@@ -0,0 +1,6 @@
+# Thebe | De Heuvel
+# Donatello 1
+# 4812 JR Breda
+
+export LOCALIZATION_MAP=${ROSE_CONFIG}/maps/rosebv_big/localization.yaml
+export NAVIGATION_MAP=${ROSE_CONFIG}/maps/rosebv_big/navigation.yaml
